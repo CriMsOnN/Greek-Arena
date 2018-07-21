d5113e9ad9c79aecd7542a1fba2e123cd2911b12
CriMsOnN <tasosmoustakis1995@gmail.com>
2018-07-21 16:32:22 +0300

    Fixed !move command

--- commands/Mods/move.js
+++ commands/Mods/move.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 
-
 exports.run = async (client, message, args) => {
     const member = message.mentions.members.first();
     const channel = message.guild.channels.find('name', 'mod-logs');
@@ -9,14 +8,14 @@ exports.run = async (client, message, args) => {
     const chan = client.channels.find("name", vc);
     if(message.channel.name != messagechannel.name) return;
     if(!member) return message.reply('You have to mention a member');
-    if(!vc) return message.reply('You have to enter the channel name');
     if(!message.member.voiceChannel) return message.reply('You have to be on a voice channel');
     if(!member.voiceChannel) return message.reply(`The member you wanna move needs to be on a voice channel`);
-    member.setVoiceChannel(chan).then(() => {
+
+    member.setVoiceChannel(message.member.voiceChannel).then(() => {
         const embed = new Discord.RichEmbed()
             .setColor(client.color)
             .setTitle(`${message.guild.name} - LOGS`)
-            .setDescription(`**${member}** moved to **${chan}** from **${message.author.username}**`)
+            .setDescription(`**${member}** moved to **${message.member.voiceChannel}** from **${message.author.username}**`)
             .setFooter(client.footer);
         channel.send(embed);
     })
