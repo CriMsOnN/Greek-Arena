5aecef492022c5e03ce1fc8ae98e41114e1278fb
CriMsOnN <tasosmoustakis1995@gmail.com>
2018-07-22 20:54:37 +0300

    !remove command added

--- commands/Mods/remove.js
+++ commands/Mods/remove.js
@@ -6,25 +6,24 @@ exports.run = async (client, message, args) => {
     const messagechannel = message.guild.channels.find('name', 'moves');
     const user = message.author.id;
     let headadminRole = message.guild.roles.find(mR => mR.name === "HEAD ADMIN");
-
     if(message.channel.name != messagechannel.name) return;
     if(!member) return message.reply('You have to mention a member');
     if(member.id === message.author.id) return message.reply('You can not move your self');
     if(!message.member.voiceChannel) return message.reply('You have to be on a voice channel');
     if(!member.voiceChannel) return message.reply(`The member you wanna move needs to be on a voice channel`);
-    if(message.member.voiceChannel === member.voiceChannel) return message.reply('You are already on the same voice channel');
     if(member.roles.some(r=>["Moderators","HEAD ADMIN", "VIOLOGIKOS"].includes(r.name))) {
         return message.reply('You cant move Moderators or Head Admins');
     }
-
-    member.setVoiceChannel(message.member.voiceChannel).then(() => {
-        const embed = new Discord.RichEmbed()
-            .setColor(client.color)
-            .setTitle(`${message.guild.name} - LOGS`)
-            .setDescription(`**${member}** moved to **${message.member.voiceChannel}** from **${message.author.username}**`)
-            .setFooter(client.footer);
-        channel.send(embed);
-    })
+    if(message.member.voiceChannel === member.voiceChannel) {
+        member.setVoiceChannel('470342165390688258').then(() => {
+            const embed = new Discord.RichEmbed()
+                .setColor(client.color)
+                .setTitle(`${message.guild.name} - LOGS`)
+                .setDescription(`**${member}** moved to **${message.member.voiceChannel}** from **${message.author.username}**`)
+                .setFooter(client.footer);
+            channel.send(embed);
+        })
+    }
 }
 
 
@@ -36,7 +35,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: "move",
-    description: "move",
-    usage: "move"
+    name: "remove",
+    description: "remove",
+    usage: "remove"
 }
